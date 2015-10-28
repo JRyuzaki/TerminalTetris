9b9aed4aeac60500573811e2633793c0fd535983
JRyuzaki <zivanovicjovan@gmail.com>
2015-10-28 11:22:26 +0100

    created file

---
+++ Tetromino.hpp
@@ -0,0 +1,20 @@
+#ifndef TETROMINO_HPP
+#define TETROMINO_HPP
+
+#include <stdio.h>
+#include <curses.h>
+
+class Tetromino{
+protected:
+	std::pair<int, int> tetrominoPiece[4];
+
+
+public:
+	void addTetrominoPiece(int ID, std::pair<int, int> pieceCoordinates){
+		if(ID > 3 || ID < 0)
+			fprintf(stderr, "[TETROMINO.HPP/ADD_TETROMINO_PIECE]: ID INVALIDE");
+
+		tetrominoPiece[ID] = pieceCoordinates;
+	}
+};
+#endif 
