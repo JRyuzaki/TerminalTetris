2a68126a2c1995f07843fce7e9ecc324da6b9846
JRyuzaki <zivanovicjovan@gmail.com>
2015-10-30 20:07:47 +0100

    Reupload

--- GameField.hpp
+++ GameField.hpp
@@ -1,16 +1,39 @@
 #ifndef GAME_FIELD
 #define GAME_FIELD
 
+#include <curses.h>
+
+#include "Tetromino.hpp"
+
 class GameField{
 private:
-	static constexpr int gamefieldWidth = 48 + 4;
-	static constexpr int gamefieldHeight = 48;
-
-	int gamefield[gamefieldHeight][gamefieldWidth];
+	int gameFieldWidth;
+	int gameFieldHeight;
 
 public:
-	GameField(){
-		
+	GameField(const int gameFieldWidth, const int gameFieldHeight){
+		this->gameFieldWidth = gameFieldWidth;
+		this->gameFieldHeight = gameFieldHeight;
+		setCursesSettings();
+	}
+
+	void drawGameField(int gameField[][52]) const;
+	void drawCurrentTetromino(Tetromino& tetromino) const;
+
+	void updateGameField(){
+		refresh();
+	}
+
+private:
+	void setCursesSettings(){
+		initscr();
+		noecho();
+		curs_set(false);
+		start_color();
+		use_default_colors();
+
+		init_pair(1, COLOR_GREEN, 0);
+		init_pair(2, COLOR_GREEN, COLOR_GREEN);
 	}
 };
 #endif 
