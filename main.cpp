c03830f761cb5427b9c15ec1e5e69a8676d46460
JRyuzaki <zivanovicjovan@gmail.com>
2015-10-30 20:10:03 +0100

    Added Seeding for rand()

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
-#include "Tetromino.hpp"
-#include "GameField.hpp"
+#include "Tetromino.hpp"    
+#include "Tetris.hpp"
+
+#include <unistd.h>	
+#include <stdlib.h>
+#include <time.h>
+
+#define GAME_UPDATE_TIME 100
 
 int main(){
+	srand(time(NULL));
+
+	Tetris tetris = Tetris{};
+
+	while(!tetris.isGameOver()){
+		tetris.updateGame();
+		//tetris.rotateRight();
+		usleep(GAME_UPDATE_TIME);
+	}
+
+	getchar();
+	endwin();
 	return 0;
-}
+}      
